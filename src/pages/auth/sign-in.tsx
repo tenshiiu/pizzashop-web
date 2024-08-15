8625f2277ddf691545f8bc7a974784dcfafd0315
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-08-15 18:02:22 -0300

    Aba de cadastro

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom"
 import { z } from "zod";
 import { toast } from "sonner";
 
@@ -37,6 +38,13 @@ export function SignIn() {
     return <>
         <Helmet title="Login"/>
     <div className="p-8">
+
+      <Button variant="ghost" asChild className="absolute right-8 top-8">
+      <Link to="/sign-up">
+        Novo estabelecimento
+      </Link>
+      </Button>
+
        <div className="flex w-[350px] flex-col justify-center gap-6">
         <div className="flex flex-col gap-2 text-center">
          <h1 className="text-2xl font-semibold tracking-tight"> 
