835d4c681b28e0c5255e0902dbe25a72ff33dfd4
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-08-15 15:44:14 -0300

    Instalação e configuração de recursos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import "./global.css";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+
 export function App() {
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+        <HelmetProvider>
+          <Helmet titleTemplate="%s | pizza.shop" />
+          <RouterProvider router={router} />
+        </HelmetProvider>
   )
 }
