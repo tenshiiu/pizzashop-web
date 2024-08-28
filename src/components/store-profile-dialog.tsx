7c4478448e47ed3306fed09a20ba5368faa5e378
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-08-28 16:41:44 -0300

    Interface otimista

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -13,7 +13,7 @@ import { toast } from "sonner"
 
 const storeProfileSchema = z.object({
     name: z.string().min(1),
-    description: z.string(),
+    description: z.string().nullable(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
@@ -40,10 +40,8 @@ export function StoreProfileDialog() {
         },
     })
 
-    const { mutateAsync: updateProfileFn } = useMutation({
-        mutationFn: updateProfile,
-        onSuccess(_, { name, description }) {
-            const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+    function updateManagedRestaurantCache({ name, description } : StoreProfileSchema) {
+        const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
 
             if (cached){
                 queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
@@ -51,6 +49,22 @@ export function StoreProfileDialog() {
                     name,
                     description,
                 })
+            }
+
+        return { cached }
+    }
+
+    const { mutateAsync: updateProfileFn } = useMutation({
+        mutationFn: updateProfile,
+        onMutate({ name, description }) {
+            updateManagedRestaurantCache({ name, description})
+
+            return { previousProfile: cached }
+        },
+
+        onError(_, __, context) {
+            if (context?.previousProfile) {
+                updateManagedRestaurantCache(context.previousProfile)
             }
         },
     })
