3666998f0fd15c0705ddba53df15742873beb9c9
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-09-12 17:00:28 -0300

    Mock de cadastro

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,9 @@
 import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { signInMock } from "./sign-in-mock";
+import { registerRestaurantMock } from "./register-restaurant-mock";
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(signInMock, registerRestaurantMock)
 
 export async function enableMSW() {
     if (env.MODE != 'test') {
